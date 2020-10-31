cc72dd44dd1c020ee60bf3e4efce6326460287c2
alfianhidayahh <62759588+alfianhidayahh@users.noreply.github.com>
2020-10-31 15:00:24 +0700

    Add project files.

---
+++ Server_AlfianHidayah_20180140006/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Server_AlfianHidayah_20180140006
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnON = new System.Windows.Forms.Button();
+            this.BtnOFF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(357, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Server";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(377, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(375, 173);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Klik";
+            // 
+            // BtnON
+            // 
+            this.BtnON.Location = new System.Drawing.Point(294, 248);
+            this.BtnON.Name = "BtnON";
+            this.BtnON.Size = new System.Drawing.Size(75, 23);
+            this.BtnON.TabIndex = 5;
+            this.BtnON.Text = "ON";
+            this.BtnON.UseVisualStyleBackColor = true;
+            this.BtnON.Click += new System.EventHandler(this.BtnON_Click);
+            // 
+            // BtnOFF
+            // 
+            this.BtnOFF.Location = new System.Drawing.Point(446, 248);
+            this.BtnOFF.Name = "BtnOFF";
+            this.BtnOFF.Size = new System.Drawing.Size(75, 23);
+            this.BtnOFF.TabIndex = 6;
+            this.BtnOFF.Text = "OFF";
+            this.BtnOFF.UseVisualStyleBackColor = true;
+            this.BtnOFF.Click += new System.EventHandler(this.BtnOFF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnOFF);
+            this.Controls.Add(this.BtnON);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnON;
+        private System.Windows.Forms.Button BtnOFF;
+    }
+}
+
---
+++ Server_AlfianHidayah_20180140006/Form1.cs
@@ -0,0 +1,45 @@
+﻿using ServiceMtk_P1_006;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_AlfianHidayah_20180140006
+{
+    public partial class Form1 : Form
+    {
+        ServiceHost hostObj = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnON_Click(object sender, EventArgs e)
+        {
+            hostObj = new ServiceHost(typeof(Matematika));
+            hostObj.Open();
+        }
+
+        private void BtnOFF_Click(object sender, EventArgs e)
+        {
+            hostObj = new ServiceHost(typeof(Matematika));
+            hostObj.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
